d380840a24620ce45a27c657fa5289170ab82cdd
dnaghitorabi1 <96211123+dnaghitorabi1@users.noreply.github.com>
2022-05-26 13:14:57 -0400

    Create unary.js

---
+++ unary.js
@@ -0,0 +1,57 @@
+// Declarations
+const MESSAGE = readline(); // original string
+console.error("MESSAGE: " + MESSAGE);
+const map = new Map([
+  ["0", [0, 0]],
+  ["1", [0]]
+]);
+// ----------------------
+
+// Main
+let messageArray = MESSAGE.split("");
+console.error("messageArray: " + messageArray);
+
+let messageArrayBinary = convertToBinary(messageArray);
+console.error("messageArrayBinary: " + messageArrayBinary);
+
+let answer = [];
+
+messageArrayBinary.forEach((item, i) => { // For each digit in the binary array,
+  if (item !== messageArrayBinary[i - 1]) { // if different from previous digit,
+    if (i !== 0) {
+      answer.push(" "); // New blocks are prefaced with a space if not on first item.
+    }
+
+    let firstBlock = map.get(item);
+
+    firstBlock.forEach((item2, j) => {
+      answer.push(item2);
+    });
+
+    answer.push(" "); // Conclude first block with a space.
+  }
+  answer.push(0);
+});
+
+answer = answer.join("");
+console.log(answer);
+// ----------------------
+
+// Functions
+function convertToBinary(array) {
+  let array2 = [];
+  array.forEach((item, i) => { // For each character in message,
+    item = item.charCodeAt(); // Convert to unicode.
+    item = item.toString(2); // Convert to binary.
+
+    while (item.length < 7) {
+      item = "0" + item;
+    }
+
+    [...item].forEach((item2, j) => {
+      array2.push(item2);
+    });
+  });
+  return array2;
+}
+// ----------------------
